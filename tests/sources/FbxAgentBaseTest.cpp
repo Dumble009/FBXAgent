a4a08d1c354274777cb4c83bb02ca9b4dfaff6b9
Dumble009 <daichi19990821@gmail.com>
2022-10-10 17:28:59 +0900

    env: tasks.jsonを変更し、テストをctestで起動するのではなく、ビルドされたGoogleTestのexeを直接実行するように変更。こうすることでテスト失敗時にメッセージを見ることが出来るようになった。

---
+++ tests/sources/FbxAgentBaseTest.cpp
@@ -0,0 +1,25 @@
+﻿#include <gtest/gtest.h>
+#include "FbxAgent.h"
+
+// FbxAgentの初期化やモデルをロードできるか等基本的な項目をテストする
+
+// FbxAgent::Initを実行してもエラーが返されないかを確認する
+TEST(FbxAgentInit, BasicAssertions)
+{
+    fbxAgent::FbxAgent agent = fbxAgent::FbxAgent();
+
+    auto ret = agent.Init();
+
+    ASSERT_EQ(ret, FBX_AGENT_SUCCESS) << "ret value is : " << ret;
+}
+
+TEST(FbxAgentLoad, BasicAssertions)
+{
+    fbxAgent::FbxAgent agent = fbxAgent::FbxAgent();
+
+    agent.Init();
+
+    auto ret = agent.Load("C:/Users/daich/Documents/FBXAgent/vikingroom");
+
+    ASSERT_EQ(ret, FBX_AGENT_SUCCESS) << "ret value is : " << ret;
+}
