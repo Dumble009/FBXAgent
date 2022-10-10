bdf2f344be01d76b5d05c726eb84db0eb900f4f9
Dumble009 <daichi19990821@gmail.com>
2022-10-10 16:44:27 +0900

    feat: 複数のテストコードをビルドする事が出来るようにtests/CMakeLists.txtを変更。問題無く複数のテスコトードがビルド、実行されることを確認

---
+++ tests/sources/hello_test2.cpp
@@ -0,0 +1,10 @@
+﻿#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest2, BasicAssertions)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world2");
+    // Expect equality.
+    EXPECT_EQ(1 + 1, 2);
+}
